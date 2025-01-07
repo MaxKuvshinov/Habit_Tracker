1e49b45c02f0dbbcb2a85d1ddce1e100290ed425
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2025-01-07 16:50:10 +0300

    views have been added to the views file

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
+from users.serializers import CustomUserSerializer
+from users.models import CustomUser
 
-# Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class CustomUserCreateAPIView(CreateAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
